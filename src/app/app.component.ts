17b692df6e6dbbe0c7f1f7212ad2bf8ff439e753
Anil Golla <akgolla1@gmail.com>
2024-07-29 08:58:40 +0530

    My practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { BodyComponent } from './components/body/body.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,BodyComponent
+    ,NavbarComponent,CategoriesComponent,CarouselComponent,CardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
